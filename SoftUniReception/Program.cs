cd3cfcc984d02365037b87831314b6c366a4254d
Emo <eiv.razgrad@gmail.com>
2018-07-02 13:35:04 +0300

    First task

---
+++ SoftUniReception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var limitOfEmployeePerHour1 = int.Parse(Console.ReadLine());
+            var limitOfEmployeePerHour2 = int.Parse(Console.ReadLine());
+            var limitOfEmployeePerHour3 = int.Parse(Console.ReadLine());
+            var totalStudents = long.Parse(Console.ReadLine());
+            //
+            var totalEmplForHour = limitOfEmployeePerHour1 + limitOfEmployeePerHour2 + limitOfEmployeePerHour3;
+            var hours = 0;
+            while (totalStudents>0)
+            {
+                totalStudents -= totalEmplForHour;
+                hours++;
+                if (hours % 4 ==0)
+                {
+                    hours++;
+                }
+            }
+          
+            Console.WriteLine($"Time needed: {hours}h.");
+
+        }
+    }
+}
