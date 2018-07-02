91f442596c4088e7eac824ac9dcef91f348024f0
Emo <eiv.razgrad@gmail.com>
2018-07-02 14:01:55 +0300

    Third and fourth => 0/100 0/100 task. Must rewrite => ToDo

---
+++ SoftUniBarIncome/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"");
+            string input = Console.ReadLine();
+
+            while (input != "end of shift")
+            {
+                var name = "";
+                var product = "";
+                var count = int.Parse();
+                var price = double.Parse();
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
