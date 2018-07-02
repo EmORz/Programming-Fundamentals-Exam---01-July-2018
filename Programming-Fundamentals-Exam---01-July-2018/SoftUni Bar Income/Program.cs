c3abae8945319648f57db3b1c190702ab49e1e3c
Emo <eiv.razgrad@gmail.com>
2018-07-02 15:13:56 +0300

    Problem 3 – SoftUni Bar Income -> edited version. Zero test are with valid result

---
+++ Programming-Fundamentals-Exam---01-July-2018/SoftUni Bar Income/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ToDo
+            Regex reg = new Regex(@"[^|$%.]*?%([A-Z][a-z]+)%[^|$%.]*?<([A-Za-z]+)>[^.|$%]*?\|(\d+)\|[^.|$%]*?(\d+\.?\d*)\$");
+            string input = Console.ReadLine();
+            var totalIncome = 0.0;
+
+            while (input != "end of shift")
+            {
+
+                if (reg.IsMatch(input))
+                {
+                    Match extrudeData = reg.Match(input);
+                    var name = extrudeData.Groups[1].Value;
+                    var product = extrudeData.Groups[2].Value;
+                    var count = int.Parse(extrudeData.Groups[3].Value);
+                    var price = double.Parse(extrudeData.Groups[4].Value);
+                    //For each valid line print on the console: "{customerName}: {product} - {totalPrice}"
+                    Console.WriteLine($"{name}: {product} - {price*count:f2}");
+                    totalIncome += (price*count);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+
+        }
+    }
+}
